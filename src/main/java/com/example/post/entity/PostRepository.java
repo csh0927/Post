e209ec79fb33ac472d42b47f4987adfb5e327cc7
csh0927 <sung060927@gmail.com>
2022-07-13 20:15:19 +0900

    게시글 리스트, 조회 Repository 메소드 추가

--- src/main/java/com/example/post/entity/PostRepository.java
+++ src/main/java/com/example/post/entity/PostRepository.java
@@ -2,9 +2,12 @@ package com.example.post.entity;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 
 
 public interface PostRepository extends CrudRepository<Post, Long> {
     Optional<Post> findById(Long id);
+    List<Post> findAllByOrderByIdDesc(Pageable page);
 }
